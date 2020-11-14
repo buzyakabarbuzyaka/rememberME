1bc09fa2b284ad9205cbd0ae48f7cbfecb58dfae
Buzov Vyahceslav <buzovv1997@gmail.com>
2020-11-14 11:58:36 +0300

    new bot old functional

--- telegram_bot/main.py
+++ telegram_bot/main.py
@@ -1,8 +1,16 @@
-import telebot
+import logging
+import telegram
 import collections
-from settings import BOT_TOKEN
+from telegram.ext import Filters, Updater, MessageHandler, CommandHandler
+from settings import TOKEN
 
-# TODO: добавить logger
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+bot = telegram.Bot(TOKEN)
+
 
 def remember():
     answer = yield "Я слушаю)"
@@ -10,40 +18,46 @@ def remember():
     return answer.text
 
 
-bot = telebot.TeleBot(BOT_TOKEN)
 handlers = collections.defaultdict(remember)
 buffer = collections.defaultdict(lambda: 'empty')
 
 
-@bot.message_handler(commands=['remember'])
-def remember_handler(message):
-    print(f'{message.from_user.username}:{message.text}')
-    telegram_id = message.from_user.id
+def remember_handler(update, context):
+    print(f'{update.message.from_user.username}:{update.message.text}')
+    print("remember")
+    telegram_id = update.message.from_user.id
 
     answer = next(handlers[telegram_id])
     # отправляем полученный ответ пользователю
     bot.send_message(chat_id=telegram_id, text=answer)
 
 
-@bot.message_handler(content_types=['text'])
-def insult(message):
-    print(f'{message.from_user.username}:{message.text}')
-    telegram_id = message.from_user.id
+def insult(update, context):
+    print(f'{update.message.from_user.username}:{update.message.text}')
+    telegram_id = update.message.from_user.id
     if telegram_id in handlers:
         # если диалог уже начат, то надо использовать .send(), чтобы
         # передать в генератор ответ пользователя
         try:
-            handlers[telegram_id].send(message)
+            handlers[telegram_id].send(update.message)
         except StopIteration:
             del handlers[telegram_id]
             bot.send_message(chat_id=telegram_id, text=buffer[telegram_id])
             del buffer[telegram_id]
             bot.send_message(chat_id=telegram_id, text="Я канеш запомнил, но ты все равно идешь нахуй.")
             return
-    bot.send_message(telegram_id, "Ди на хуй")
+
+    update.message.reply_text('Ди на хуй')
+
+
+def main():
+    updater = Updater(TOKEN, use_context=True)
+    updater.dispatcher.add_handler(CommandHandler('remember', remember_handler))
+    updater.dispatcher.add_handler(MessageHandler(Filters.text, insult))
+
+    updater.start_polling()
+    updater.idle()
 
 
 if __name__ == '__main__':
-    # bot.polling(none_stop=True, interval=1)
-    bot.start_polling()
-    bot.idle()
+    main()
