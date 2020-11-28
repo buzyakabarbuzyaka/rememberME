78de27ee00849430e1a6a0f772fd3260875f1a93
Buzov Vyahceslav <buzovv1997@gmail.com>
2020-11-28 15:36:53 +0300

    dbs new models

--- fast_api/app/models.py
+++ fast_api/app/models.py
@@ -5,23 +5,55 @@ from sqlalchemy.orm import relationship
 from app.database import Base, engine
 
 
+# class User(Base):
+#     __tablename__ = "users"
+#
+#     id = Column(Integer, primary_key=True, index=True)
+#     telegram_id = Column(String, unique=True, index=True)
+#
+#     items = relationship("Item", back_populates="owner")
+#
+#
+# class Item(Base):
+#     __tablename__ = "items"
+#
+#     id = Column(Integer, primary_key=True, index=True)
+#     content = Column(String, index=True)
+#     owner_id = Column(Integer, ForeignKey("users.id"))
+#
+#     owner = relationship("User", back_populates="items")
+
+
 class User(Base):
     __tablename__ = "users"
-
     id = Column(Integer, primary_key=True, index=True)
     telegram_id = Column(String, unique=True, index=True)
+    # id = Column(String, primary_key=True, index=True)  # telegram_id
+    cards = relationship("Card", back_populates="user")
 
-    items = relationship("Item", back_populates="owner")
+
+class Card(Base):
+    __tablename__ = "cards"
+
+    id = Column(Integer, primary_key=True, index=True)
+    card_name = Column(String, index=True)  # TODO: add uniqueness to user_id, card_name
+
+    user_id = Column(Integer, ForeignKey("users.id"))
+    user = relationship("User", back_populates="cards")
+
+    items = relationship("Item", back_populates="card")
 
 
 class Item(Base):
     __tablename__ = "items"
 
     id = Column(Integer, primary_key=True, index=True)
-    content = Column(String, index=True)
-    owner_id = Column(Integer, ForeignKey("users.id"))
+    card_id = Column(Integer, ForeignKey("cards.id"))
 
-    owner = relationship("User", back_populates="items")
+    question = Column(String, index=True)  # TODO: add uniqueness to question, card_id
+    answer = Column(String)
+
+    card = relationship("Card", back_populates="items")
 
 
 if __name__ == '__main__':
